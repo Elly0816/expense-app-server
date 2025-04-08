4605501cb7ed199d79c863ec6c756ba34e09fdcf
Eleazar Udo <elzoremmanuel@gmail.com>
2025-04-08 02:24:29 +0100

    started with google oAuth

---
+++ src/auth/passport.auth.ts
@@ -0,0 +1,12 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    () => {}
+  )
+);
