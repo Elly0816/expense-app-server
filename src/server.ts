897fb3cbaedd0b34d12d1ae46f740663191a37eb
Eleazar Udo <elzoremmanuel@gmail.com>
2025-05-30 14:59:24 +0100

    request debugging

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,32 @@ import { expense } from './routes/expense/expense';
 import auth from './routes/auth/auth';
 import { googleOauth } from './middlewares/auth';
 import { logger } from 'hono/logger';
+import type { Context, Next } from 'hono';
+import { getConnInfo } from 'hono/bun';
 
 console.log('Environment Variables:', {
   CORS_ORIGIN: process.env.CORS_ORIGIN,
   PORT: process.env.PORT,
 });
 
+app.use(async (c: Context, next: Next): Promise<void> => {
+  const req = c.req;
+
+  console.log('=== REQUEST DEBUGGING ===');
+  const info = getConnInfo(c);
+  console.log(`The connection info is: `);
+  console.log(info);
+  console.log('\n');
+  // console.log('req.protocol:', req.protocol);
+  // console.log('req.secure:', req.secure);
+  // console.log('req.get("host"):', req.get('host'));
+
+  console.log('req.get("x-forwarded-proto"):', req.header('x-forwarded-proto'));
+  console.log('req.get("x-forwarded-for"):', req.header('x-forwarded-for'));
+  console.log('req.headers:', JSON.stringify(req.header(), null, 2));
+  console.log('========================');
+  next();
+});
 app.use(cors({ origin: Bun.env.CORS_ORIGIN as string, credentials: true }));
 app.use(logger());
 //This middleware initiates the oAuth process
